0816b3ae2e76d3b0824f0aabb841909b0512eecf
Daniel Sagi <danielsagi2009@gmail.com>
2017-11-02 17:59:39 +0200

    Form posts now working on both login and register. for now the data sent is just printed on the server. Further DB integration needs to be implemented, for pushing and pulling to and from a secure DB.

--- server.py
+++ server.py
@@ -1,14 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        print dict(request.form)
     return render_template("index.html")
 
 @app.route('/register.html', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        print dict(request.form)
     return render_template("register.html")
 
 if __name__=="__main__":
